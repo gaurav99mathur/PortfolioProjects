4e604b792fca99d09a97e82dff47b8023ae0e00d
gaurav99mathur <121626923+gaurav99mathur@users.noreply.github.com>
2023-01-13 16:11:20 +0530

    Update Rape Cases in India 2001-2023.sql

--- Rape Cases in India 2001-2023.sql
+++ Rape Cases in India 2001-2023.sql
@@ -1,12 +1,15 @@
-select [States/UTs/Cities] from IndiaRapeCaseDatabase..['RapeCases2001-2011']
 
+-- To Show Year Wise Incest Rape Cases In India
 select Distinct(YEAR), SUM([No# of Cases Reported (Incest Rape Cases)]) as TotalIncestRapeCaseReported,SUM([No# of Cases Reported (Other Rape Cases)]) as TotalOtherRapeCaseReported, SUM([No# of Cases Reported (Total Rape Cases)]) as TotalRapeCaseReported from IndiaRapeCaseDatabase..['RapeCases2001-2011']
 group by Year
 
+-- To Show State Wise Incest Rape Cases In India
 
 select Distinct([States/UTs/Cities]), SUM([No# of Cases Reported (Incest Rape Cases)]) as TotalIncestRapeCaseReported, SUM([No# of Cases Reported (Other Rape Cases)]) as TotalOtherRapeCaseReported, SUM([No# of Cases Reported (Total Rape Cases)]) as TotalRapeCaseReported from IndiaRapeCaseDatabase..['RapeCases2001-2011']
 group by [States/UTs/Cities]
 order by TotalRapeCaseReported DESC
+
+-- To show Number Of Cases in Each State in India Age-Group Wise
 
 select Distinct([States/UTs/Cities]), SUM([No# of Victims (Total Rape Cases) upto 10 Years]) as TotalVictimsUpto10Years, 
 SUM([No# of Victims (Total Rape Cases) - 10-14 Years]) as TotalVictimsBetween10To14Years,
@@ -16,6 +19,8 @@ SUM([No# of Victims (Total Rape Cases) - 30 - 50 Years]) as TotalVictimsBetween3
 SUM([No# of Victims (Total Rape Cases) above 50 Years]) as TotalVictimsAbove50Years
 from IndiaRapeCaseDatabase..['RapeCases2001-2011']
 group by [States/UTs/Cities]
+
+-- To show Number Of Cases in Each Year in India Age-Group Wise
 
 select Distinct(Year), SUM([No# of Victims (Total Rape Cases) upto 10 Years]) as TotalVictimsUpto10Years, 
 SUM([No# of Victims (Total Rape Cases) - 10-14 Years]) as TotalVictimsBetween10To14Years,
